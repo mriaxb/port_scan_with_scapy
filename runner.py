7e8891b817a02caca1c08d3e7c389f4cc1830014
Maria <maria@kali>
2024-01-08 17:03:26 -0300

    ajustes

--- runner.py
+++ runner.py
@@ -1,21 +1,11 @@
-#####################################################
-# Copyright (c) 2014 Diogo Barradas                 #
-#                                                   #
-# See the file LICENSE.txt for copying permission.  #
-#####################################################
-
-#!/usr/bin/env python
-
 import argparse
 import sys
-import socket
 
-import scanner
+import scanner as scanner
 from scanner import scan
 
 
 parser = argparse.ArgumentParser(prog='runner')
-parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 parser.add_argument('-p', '--ports', nargs=2, required=True, help='Port interval to scan')
 parser.add_argument('-t', '--target', required=True, help='Target host')
 parser.add_argument('-m', '--mode', nargs=1, required=True, help='scan mode: 1-syn, 2-xmas, 3-fin, 4-null, 5-ack ')
@@ -28,7 +18,7 @@ try:
     endPort = int(args.ports[1])
     assert beginPort >= 0 and endPort >= 0 and beginPort <= endPort
 except AssertionError:
-    print ("[ERROR] O range de portas é inválido")
+    print ("[ERROR] The port range is invalid")
     sys.exit()
 
 
