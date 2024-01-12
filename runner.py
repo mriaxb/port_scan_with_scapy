e8d16da81f3330736ffcbd4c25d422848deb7509
Maria <maria@kali>
2024-01-12 11:15:04 -0300

    finalização dos métodos

--- runner.py
+++ runner.py
@@ -8,7 +8,7 @@ from scanner import scan
 parser = argparse.ArgumentParser(prog='runner')
 parser.add_argument('-p', '--ports', nargs=2, required=True, help='Port interval to scan')
 parser.add_argument('-t', '--target', required=True, help='Target host')
-parser.add_argument('-m', '--mode', nargs=1, required=True, help='scan mode: 1-syn, 2-xmas, 3-fin, 4-null, 5-ack ')
+parser.add_argument('-m', '--mode', nargs=1, required=True, help='scan mode: 1-syn, 2-fin, 3-ack ')
 
 
 args = parser.parse_args()
